3571ff861308f645ffcaef6d59c96e6d685fe56b
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-03-20 17:33:13 -0300

    Logger, middleware, mapstatetoprops and mapDispatchToProps

--- practica-redux-react/src/components/ComprarPokemon.jsx
+++ practica-redux-react/src/components/ComprarPokemon.jsx
@@ -1,10 +1,34 @@
-const ComprarPokemon = () => {
+// CAPITULO 14. Vamos a crear el dispatch para este componente
+import { connect } from 'react-redux';
+import { buy_pokemon_action, return_pokemon_action } from '../redux/actions/gameShopAction';
+
+const ComprarPokemon = (props) => {
     return (
         <div>
-            <button className="btn btn-dark btn-sm mb-2">Comprar Pokemom</button>
-            <button className="btn btn-dark btn-sm">Regresar Pokemom</button>
+            <button 
+                className="btn btn-dark btn-sm mb-2"
+                onClick={() => props.buy_pokemon_action(1)}
+            >
+                Comprar Pokemom
+            </button>
+            <button 
+            className="btn btn-dark btn-sm"
+            onClick={() => props.return_pokemon_action(1)}
+            >
+                Regresar Pokemom
+            </button>
         </div>
     );
 }
 
-export default ComprarPokemon;
+const mapDispatchToProps = {
+    buy_pokemon_action, 
+    return_pokemon_action
+};
+
+
+export default connect(null, mapDispatchToProps)(ComprarPokemon);
+// CAPITULO 14. El primer parametro de connect es null porque ese 
+// campo esta relacionado a los estados y en este componente no tenemos
+// relacionado nada a ello y en el segundo parametro colocamos mapDispatchToProps
+// porque este esta relacionado al dispatch 
--- practica-redux-react/src/redux/store.js
+++ practica-redux-react/src/redux/store.js
@@ -2,7 +2,18 @@
 
 import { createStore } from "redux";
 import rootReducers from "./reducers/rootReducers";
+// CAPITULO 12. Agregamos el middleware composeWithDevTools para que funcione el STORE en el navegador
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = createStore(rootReducers);
+
+
+// CAPITULO 12. Luego de instalar la dependencia @redux-devtools/extension,
+// vamos a agregar la siguiente linea para poder hacer funcionar el
+// STORE en el navegador.
+const store = createStore(rootReducers, composeWithDevTools());
 
 export default store;
+
+// CAPITULO 12. Una vez modificada la linea 13 (const store = createStore(rootReducers, composeWithDevTools());)
+// clickeamos en inspeccionar y tenemos la opcion Redux donde se encuentran Elements, 
+// Console, etc
