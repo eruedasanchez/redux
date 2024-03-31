bd8876f99ed2c0b8b1cca2fc7ea9bdfbc3b50a49
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-03-31 11:52:20 -0300

    Add styles with Tailwind to the CRUD

--- react-rtq-crud/src/App.js
+++ react-rtq-crud/src/App.js
@@ -5,14 +5,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<TasksList/>}/>
-          <Route path='/create-task' element={<TasksForm/>}/>
-          <Route path='/edit-task/:id' element={<TasksForm/>}/>
-        </Routes>
-      </BrowserRouter>
+    <div className='bg-zinc-900 h-screen text-white'>
+      <div className='flex justify-center items-center h-full'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TasksList/>}/>
+            <Route path='/create-task' element={<TasksForm/>}/>
+            <Route path='/edit-task/:id' element={<TasksForm/>}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- react-rtq-crud/src/components/TasksForm.js
+++ react-rtq-crud/src/components/TasksForm.js
@@ -44,24 +44,28 @@ const TasksForm = () => {
         if (id) {
             setTask(tasks.find(task => task.id === id))
         }
-    }, []);
+    }, [id, tasks]);
     
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+            <label htmlFor="title" className="block text-xs font-bold mb-2">Task:</label>
             <input 
                 name="title" 
                 type='text' 
                 placeholder='title' 
                 onChange={handleChange}
                 value={task.title}
+                className="w-full p-2 rounded-md bg-zinc-600 mb-2"
             />
+            <label htmlFor="description" className="block text-xs font-bold mb-2">Description:</label>
             <textarea 
                 name='description' 
                 placeholder='description' 
                 onChange={handleChange}
                 value={task.description}
+                className="w-full p-2 rounded-md bg-zinc-600 mb-2"
             />
-            <button>Save</button>
+            <button className="bg-indigo-600 px-2 py-1">Save</button>
         </form>
     )
 }
