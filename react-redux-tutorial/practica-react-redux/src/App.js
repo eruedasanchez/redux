06b2e096babdb17583c9eb781deb79a808c5e28e
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-04-01 14:21:18 -0300

    Inicio Proyecto Buscador

--- react-redux-tutorial/practica-react-redux/src/App.js
+++ react-redux-tutorial/practica-react-redux/src/App.js
@@ -5,6 +5,8 @@ import CantidadPokemonFunction from './components/CantidadPokemonFunction';
 import ComprarPokemonFunction from './components/ComprarPokemonFunction';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import BuscadorPokemon from './components/buscador/BuscadorPokemon';
+import ResultadoPokemon from './components/buscador/ResultadoPokemon';
 
 function App() {
   return (
@@ -27,6 +29,12 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className='col-12 mt-4 border-top pt-3'>
+            <BuscadorPokemon/>
+          </div>
+          <div className='col-12'>
+            <ResultadoPokemon/>
           </div>
         </div>
       </div>
---
+++ react-redux-tutorial/practica-react-redux/src/components/buscador/ResultadoPokemon.jsx
@@ -0,0 +1,15 @@
+const ResultadoPokemon = () => {
+    return (
+        <div>
+            <h3 className="">Resultado:</h3>
+            <div className="text-warning">Buscando...</div>
+            <div className="text-success">
+                <img src="#" alt=""/>
+                <span>Pikachu</span>
+            </div>
+            <span className="text-danger">Error</span>
+        </div>
+    );
+}
+
+export default ResultadoPokemon;
