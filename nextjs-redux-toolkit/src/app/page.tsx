de9d8c8f7840db5a136ef24e05c20ad1a30577a0
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2024-04-02 13:51:01 -0300

    Agregando estilos al proyecto

--- nextjs-redux-toolkit/src/app/page.tsx
+++ nextjs-redux-toolkit/src/app/page.tsx
@@ -14,19 +14,33 @@ function HomePage() {
   
   return (
     <div>
-      <h1>Total: {count}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <br/>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      {
-        data?.map(user => (
-          <div key={user.id}>
-            <p>{user.name}</p>
-            <p>{user.username}</p>
-            <p>{user.email}</p>
-          </div>
-        ))
-      }
+      <h1 className="text-center text-2xl">Total: {count}</h1>
+      <div className="flex justify-center gap-2 mb-4">
+        <button
+          className="bg-green-500 px-3 py-2 rounded-md" 
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <br/>
+        <button 
+          className="bg-blue-500 px-3 py-2 rounded-md" 
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+      <div className="grid grid-cols-3 mx-auto gap-3">
+        {
+          data?.map(user => (
+            <div key={user.id} className="bg-zinc-400 p-4">
+              <p>{user.name}</p>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
